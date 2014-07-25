d92ea040bb2fc3b223a076f4a102c3a52f78d490
Ilie Circiumaru <ilie.circiumaru@deeplace.md>
2014-07-25 18:13:25 +0300

    Work in progress #6773 @5h without cookies and optimized queries. Added analogous IOS to Android

---
+++ README.md
@@ -0,0 +1,2 @@
+Descriem dependența de tip soft fața de modulul imagecache.
+ășțîâ
--- addtohomescreen_script.js
+++ addtohomescreen_script.js
@@ -1,61 +1,74 @@
 
+/**
+ * Drupal.settings.addToHome - object with properties:
+ *   addtohome_app_name -
+ *   addtohome_display_times -
+ *   addtohome_actions -
+ *   addtohome_second_time -
+ *   addtohome_show_after -
+ *   addtohome_lifespan -
+ *   addtohome_display_pace -
+ *   addtohome_message -
+ *   addtohome_detect -
+ *   addtohome_andr_message-
+ *   addtohome_andr_link -
+ *   addtohome_ios_icon -
+ *   image_paths -
+ *   path -
+ *  ios_use_msgbox -
+ */
 Drupal.behaviors.addToHome = function(context){
-  var app_name =  Drupal.settings.addToHome.app_name;
-  var display_times =  Drupal.settings.addToHome.display_times;
-  var actions =  Drupal.settings.addToHome.actions;
-  var second_time =  Drupal.settings.addToHome.second_time;
-  var show_after =  Drupal.settings.addToHome.show_after;
-  var lifespan =  Drupal.settings.addToHome.lifespan;
-  var display_pace =  Drupal.settings.addToHome.display_pace;
-  var message =  Drupal.settings.addToHome.message;
-  var detect =  Drupal.settings.addToHome.detect;
-  var andr_message=  Drupal.settings.addToHome.andr_message;
-  var andr_link =  Drupal.settings.addToHome.andr_link;
-  var ios_icon = Drupal.settings.addToHome.ios_icon;
-  var imagePaths =  Drupal.settings.addToHome.image_paths;
-  var path = Drupal.settings.addToHome.path;
-  
-  var isAndroid = /android/i.test(navigator.userAgent.toLowerCase());
-  var isIOS = /iphone|ipad.*safari/i.test(navigator.userAgent.toLowerCase());
-  var isStandAlone = (("standalone" in window.navigator) && !window.navigator.standalone) ? true : false; 
+  var settings = Drupal.settings.addToHome;
+  console.log(settings.image_paths);
+  var isAndroid = /android/i.test(navigator.userAgent.toLowerCase()),
+      isIOS = /iphone|ipad.*safari/i.test(navigator.userAgent.toLowerCase()),
+      isStandAlone = (("standalone" in window.navigator) && !window.navigator.standalone) ? true : false; 
   
   if(isAndroid){
-    var msgMob = '<div class="mobile-suggest">';
-    msgMob+=    '<div class="mobile-suggest-input">';
-    msgMob+=       '<div class="refuse-mobile-cont">'
-    msgMob+=          '<img class="refuse-mobile" src="'+path+'/img/exit-mob-install.png" alt="" title=""/>';
-    msgMob+=        '</div>';
-    msgMob+=       '<div class="mobile-suggest-text">';
-    msgMob+=         '<h5>'+app_name+'</h5>';
-    msgMob+=         '<p><a href="'+andr_link+'" target="_blank">'+andr_message+'</p>';
-    msgMob+=       '</div>';
-    msgMob+=     '</div>';
-    msgMob+=   '</div>';
-    jQuery("body", context).prepend(msgMob);
-    if(imagePaths.and_icon != null){
-      jQuery(".mobile-suggest .mobile-suggest-text",context)
-        .css("background","url("+imagePaths.and_icon+") no-repeat center right");
-    }
-
+    insertMes(settings.path, settings.addtohome_app_name, settings.addtohome_andr_message, settings.image_paths.addtohome_andr_icon, settings.andr_link);
   }else if(isIOS){
-    //detect if we are in full screen mode
-    if(isStandAlone){
-      insertMes(path,app_name,message,imagePaths.and_icon);
-      addMeta(imagePaths.ios_icon,'0');
-      addMeta(imagePaths.ios_ipad_icon,'76');
-      addMeta(imagePaths.ios_iphone_retina,'120');
-      addMeta(imagePaths.ios_ipad_retina,'152');
-      jQuery("head",context).append('<meta name="apple-mobile-web-app-title" content="'+app_name+'">')
+    jQuery("head",context).append('<meta name="apple-mobile-web-app-title" content="'+settings.addtohome_app_name+'">');
+    addMeta(settings.image_paths.addtohome_ios_icon,'0');
+    addMeta(settings.image_paths.addtohome_ios_ipad_icon,'76');
+    addMeta(settings.image_paths.addtohome_ios_iphone_retina,'120');
+    addMeta(settings.image_paths.addtohome_ios_ipad_retina,'152');
+
+    if(settings.addtohome_ios_use_msgbox){
+      if(isStandAlone){
+        //detect if we are in full screen mode
+        insertMes(settings.path, settings.addtohome_app_name, settings.addtohome_message, settings.image_paths.addtohome_andr_icon, null);
+      }
+    }else{
+      
+      addToHomescreen({  
+        appID: settings.addtohome_app_name,  // local storage name (no need to change)
+        fontSize: 15,       // base font size, used to properly resize the popup based on viewport scale factor
+        debug: false,       // override browser checks
+        modal: settings.addtohome_actions,       // prevent further actions until the message is closed
+        mandatory: false,     // you can't proceed if you don't add the app to the homescreen
+        autostart: true,      // show the message automatically
+        skipFirstVisit: settings.addtohome_second_time,    // show only to returning visitors (ie: skip the first time you visit)
+        startDelay: parseInt(settings.addtohome_show_after),        // display the message after that many seconds from page load
+        addtohome_lifespan:parseInt(settings.addtohome_lifespan),      // life of the message in seconds
+        displayPace:parseInt(settings.addtohome_display_pace),      // minutes before the message is shown again (0: display every time, default 24 hours)
+        maxDisplayCount: parseInt(settings.addtohome_display_times),     // absolute maximum number of times the message will be shown to the user (0: no limit)
+        icon: true,         // add touch icon to the message
+        message:settings.addtohome_message,        // the message can be customized
+        validLocation: [],      // list of pages where the message will be shown (array of regexes)
+        onInit: null,       // executed on instance creation
+        onShow: null,       // executed when the message is shown
+        onRemove: null,       // executed when the message is removed
+        onAdd: null,        // when the application is launched the first time from the homescreen (guesstimate)
+        onPrivate: null,      // executed if user is in private mode
+        detectHomescreen: settings.addtohome_detect // try to detect if the site has been added to the homescreen (false | true | 'hash' | 'queryString' | 'smartURL')
+      
+      });
     }
   }
-  
-
-  jQuery('img.refuse-mobile',context).live('click',function(){
-    jQuery('div.mobile-suggest',context).hide();
-  })
 
 
-  function addMeta($x,$size){
+
+  function addMeta($x, $size){
     if($x!=null){
       if($size!='0'){
         jQuery("head",context)
@@ -67,24 +80,33 @@ Drupal.behaviors.addToHome = function(context){
     }
   }
 
-  function insertMes($path,$app_name,$message,$imagePaths){
+  function insertMes($path, $addtohome_app_name, $message, $imagePaths, $link){
   var msgMob = '<div class="mobile-suggest">';
     msgMob+=    '<div class="mobile-suggest-input">';
     msgMob+=       '<div class="refuse-mobile-cont">'
     msgMob+=          '<img class="refuse-mobile" src="'+$path+'/img/exit-mob-install.png" alt="" title=""/>';
     msgMob+=        '</div>';
     msgMob+=       '<div class="mobile-suggest-text">';
-    msgMob+=         '<h5>'+$app_name+'</h5>';
-    msgMob+=         '<p>'+$message+'</p>';
+    msgMob+=         '<h5>'+$addtohome_app_name+'</h5>';
+    if($link==null){
+      msgMob+=         '<p>'+$message+'</p>';
+    }
+    else{
+      msgMob+=         '<p><a href="'+$link+'" target="_blank">'+$message+'</p>';
+    }
     msgMob+=       '</div>';
     msgMob+=     '</div>';
     msgMob+=   '</div>';
+
 
     jQuery("body", context).prepend(msgMob);
     if($imagePaths != null){
       jQuery(".mobile-suggest .mobile-suggest-text",context)
         .css("background","url("+$imagePaths+") no-repeat center right");
     }
+    jQuery('img.refuse-mobile',context).bind('click',function(){
+      jQuery('div.mobile-suggest',context).hide();
+    });
   }
 }
 
